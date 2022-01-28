e33cc91853797956ecbe9c13d525ddb72cc6067b
markusidelev <markusidelev@gmail.com>
2022-01-28 17:48:53 +0200

    many minor fixes

--- js/common.js
+++ js/common.js
@@ -1,22 +1,36 @@
 topbar.hide()
 
 
-$("nav a").not(":first").click(function(e)	{
-	e.preventDefault();
-	$('ul.navig').removeClass('open');
-	$('.button_menu').removeClass('change');
-	var href=$(this).attr("href");
-	$("html,body").animate({scrollTop:$(href).offset().top},1000);
-});
+// $("nav a").not(":first").click(function(e)	{
+// 	e.preventDefault();
+// 	$('ul.navig').removeClass('open');
+// 	$('.button_menu').removeClass('change');
+// 	var href=$(this).attr("href");
+// 	$("html,body").animate({scrollTop:$(href).offset().top},1000);
+// });
 
 
 
-$(".banner a").click(function(e)	{
-	e.preventDefault();
-	var href=$(this).attr("href");
-	$("html,body").animate({scrollTop:$(href).offset().top},1000);
-});
+// $(".banner a").click(function(e)	{
+// 	e.preventDefault();
+// 	var href=$(this).attr("href");
+// 	$("html,body").animate({scrollTop:$(href).offset().top},1000);
+// });
 
 $(".animated").hover(function(){
 	$(".animated").toggleClass("rubberBand infinite");
 });
+
+
+
+
+const arrowBtn = document.querySelector("#form_button")
+const form = document.querySelector(".fbform")
+const circleImg = document.querySelector("#circle_img")
+
+
+arrowBtn.addEventListener('click', function() {
+	form.classList.toggle('active')
+	circleImg.classList.toggle('active')
+
+})
